9bbf48e3243c725140e2e0ce72c6f5f269f430dd
sunshj <47186980+sunshj@users.noreply.github.com>
2025-03-22 19:22:34 +0800

    feat: add VSCode settings configuration for Prettier

--- src/commands/config/stages/prettier.ts
+++ src/commands/config/stages/prettier.ts
@@ -1,9 +1,15 @@
 import config from '#config.js'
-import { loadPackageJson, patchUpdate } from '#utils.js'
+import { loadPackageJson, loadVSCodeSettings, patchUpdate } from '#utils.js'
 import type { ConfigureContext } from '..'
 
 export async function configurePrettier(ctx: ConfigureContext) {
   if (!ctx.selectedPkgs.includes('prettier')) return
+
+  // add config to vscode settings
+  const [vscodeSettings, saveVscodeSettings] = await loadVSCodeSettings(ctx.cwd)
+  patchUpdate(vscodeSettings, 'editor.defaultFormatter', 'esbenp.prettier-vscode')
+  await saveVscodeSettings()
+
   // add config and scripts to package.json
   const [pkgJSON, savePkgJSON] = await loadPackageJson(ctx.cwd)
 
